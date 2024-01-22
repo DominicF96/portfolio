a1cca80a62d5304f22f6b45d3ee3d1d1db942ee7
Dominic Fournier <me@dominicfournier.com>
2024-01-21 19:45:27 -0500

    Fixed OG images.

--- app/[locale]/case/eosnation_ens/metadata.ts
+++ app/[locale]/case/eosnation_ens/metadata.ts
@@ -25,21 +25,21 @@ export default function generateMetadata({
       images: [
         {
           // Facebook/LinkedIn OG
-          url: "/image/og/home/1200x630.png",
+          url: "/images/og/home/1200x630.png",
           width: 1200,
           height: 630,
           alt: t[locale].title,
         },
         {
           // Instagram OG
-          url: "/image/og/home/800x1000.png",
+          url: "/images/og/home/800x1000.png",
           width: 800,
           height: 1000,
           alt: t[locale].title,
         },
         {
           // Twitter OG
-          url: "/image/og/home/800x418.png",
+          url: "/images/og/home/800x418.png",
           width: 800,
           height: 418,
           alt: t[locale].title,
