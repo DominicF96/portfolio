35f5914e1ec5034fcc1374cd514fc8bf0c32af60
Dominic Fournier <me@dominicfournier.com>
2024-04-30 11:05:57 -0400

    Added rotating bg blog

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import { Locale, locales } from "@/i18n.config";
 import Footer from "@/components/Footer";
 import Newsletter from "@/components/Newsletter";
+import Image from "next/image";
 
 type Props = {
   children: React.ReactNode;
@@ -12,6 +13,14 @@ function LocalizedLayout({ children }: Props) {
   return (
     <div>
       <Navbar locale={locales[0]} />
+      <Image
+        src="/vectors/bg.svg"
+        height={1200}
+        width={1200}
+        alt=""
+        className="fixed z-0 -top-96 -right-96 blur-[200px] animate-spin"
+        style={{ animationDuration: "30s" }}
+      />
       {children}
       <Newsletter locale={locales[0]} />
       <Footer locale={locales[0]} />
