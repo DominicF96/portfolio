0eb779bba90eadc514ab5be6e8b7936e26e18e43
Dominic Fournier <me@dominicfournier.com>
2024-06-10 12:29:19 -0400

    Removed remaining contentlayer related stuff

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const { withContentlayer } = require('next-contentlayer')
-
-/** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true, swcMinify: true }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig
