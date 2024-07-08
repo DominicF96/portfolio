ec84222f8a3649571303ac14d626d6bdd407a6e5
Dominic Fournier <me@dominicfournier.com>
2024-07-07 20:58:08 -0400

    Fix api route

--- app/api/glossary/route.ts
+++ app/api/glossary/route.ts
@@ -1,6 +1,5 @@
 import { GLOSSARY } from "@/constants/glossary";
 
-export async function GET(req: Request) {}
-export default async function handler(req: Request, res: any) {
+export async function GET(req: Request, res: any) {
   res.status(200).json(GLOSSARY);
 }
